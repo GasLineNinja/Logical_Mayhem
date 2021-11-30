82a23ac2e9cd7819cfbd5df7602b05721b83af88
MikeStrand80 <mdstrand@uwm.edu>
2021-11-29 22:50:54 -0600

    added classes folder, models,views and html files

--- project_apps/models.py
+++ project_apps/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    name = models.CharField(max_length=25)
+    password = models.CharField(max_length=25)
+
