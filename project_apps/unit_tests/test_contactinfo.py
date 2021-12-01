e8f5af8de44c013d4100766f46be05dfdb3fe477
danny <boettc74@uwm.edu>
2021-12-01 16:01:03 -0600

    Moved Unittest folder, Added test_login and tests_contactinfo
    Added contact info classes in models.py
    
    Added more getters and setters to classes in models.py
    
    Added tests to test_contactinfo.py

--- project_apps/unit_tests/test_contactinfo.py
+++ project_apps/unit_tests/test_contactinfo.py
@@ -1,21 +1,29 @@
 import unittest
-from project_apps.models import ContactInfo
+from project_apps.models import ContactEmail, ContactPhone
 
-class MyContactsTests(unittest.TestCase):
+class test_ContactInfo(unittest.TestCase):
     def test_validEmail(self):
         pass
 
     def test_numEmail(self):
-        pass
+        with self.assertRaises(BaseException, msg="Putting a number in email does not give error"):
+            ContactEmail.set(self, 123345)
 
     def test_longEmail(self):
-        pass
+        with self.assertRaises(BaseException, msg="Putting a string over 25 chars in email does not give error"):
+            ContactEmail.set(self, "abcdefghijklmnopqrstuvwxyz@uwm.edu")
 
     def test_validPhone(self):
         pass
 
     def test_charPhone(self):
-        pass
+        with self.assertRaises(BaseException, msg="Putting a string in phone number does not give error"):
+            ContactPhone.set(self, "Hello")
 
     def test_longPhone(self):
-        pass
+        with self.assertRaises(BaseException, msg="Putting a int over 10 in phone number does not give error"):
+            ContactPhone.set(self, 12345678901234567890)
+
+    def test_shortPhone(self):
+        with self.assertRaises(BaseException, msg="Putting a int under 10 in phone number does not give error"):
+            ContactPhone.set(self, 12345)
