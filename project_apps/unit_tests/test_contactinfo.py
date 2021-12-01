14522c6a69792dc05bafb420576e2c2c92c8484b
danny <boettc74@uwm.edu>
2021-12-01 15:26:34 -0600

    Moved Unittest folder, Added test_login and tests_contactinfo

---
+++ project_apps/unit_tests/test_contactinfo.py
@@ -0,0 +1,4 @@
+import unittest
+
+class MyContactsTests(unittest.TestCase):
+    def
--- project_apps/unit_tests/tests.py
+++ project_apps/unit_tests/tests.py
@@ -1,3 +1,3 @@
 from django.test import TestCase
-
+from project_apps.models import Users
 # Create your tests here.
