6be9b5bfd37e8cb8d0d57e441b11b478f2c19677
danny <boettc74@uwm.edu>
2021-12-02 16:30:55 -0600

    Added skeleton interfacces for contact info

---
+++ final_project/instructorEditContact.py
@@ -0,0 +1,8 @@
+from editContactInfo import EditContactInfoInterface
+
+class instructorEditContact(object, EditContactInfoInterface):
+    def editInfo(editContact):
+        pass
+
+    def edit(contactInfo):
+        pass
