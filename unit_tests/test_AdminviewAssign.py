7b20a5e8296b81f1281681183d343106121d7b3e
syedaafrah <68800827+syedaafrah@users.noreply.github.com>
2021-12-02 18:54:32 -0600

    Made the different tests and classes

---
+++ unit_tests/test_AdminviewAssign.py
@@ -0,0 +1,29 @@
+from final_project import AdminviewAssign
+import unittest
+
+class test_init(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test_viewCourseSelected(self):
+        pass
+    def test_assignmentsExists(self):
+        pass
+
+class test_viewAssignments(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_assignmentsViewed(self):
+        pass
+
+    def test_rightCourse(self):
+        pass
+
+    def test_courseExists(self):
+        pass
+
+    def test_assignmentExists(self):
+        pass
+
+
+
