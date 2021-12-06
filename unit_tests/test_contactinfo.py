e453c212df062d36a7806079439a32b72a31a2e2
danny <boettc74@uwm.edu>
2021-12-06 10:24:25 -0600

    Added Contact Tests

--- unit_tests/test_contactinfo.py
+++ unit_tests/test_contactinfo.py
@@ -3,7 +3,7 @@ from project_apps.models import ContactEmail, ContactPhone
 
 class test_ContactInfo(unittest.TestCase):
     def test_validEmail(self):
-        pass
+        self.assertEquals(True, ContactEmail.set(self, "boettc74@uwm.edu"))
 
     def test_numEmail(self):
         with self.assertRaises(BaseException, msg="Putting a number in email does not give error"):
@@ -14,7 +14,7 @@ class test_ContactInfo(unittest.TestCase):
             ContactEmail.set(self, "abcdefghijklmnopqrstuvwxyz@uwm.edu")
 
     def test_validPhone(self):
-        pass
+        self.assertEquals(True, ContactPhone.set(self, 2623333333))
 
     def test_charPhone(self):
         with self.assertRaises(BaseException, msg="Putting a string in phone number does not give error"):
