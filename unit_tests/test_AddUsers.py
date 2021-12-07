faef50b55c307bc7e917e72d43d2e4394012ac0d
Kailee Ottman <43862300+The-Little-0tter@users.noreply.github.com>
2021-12-07 12:16:01 -0600

    Changed things to fit with the merge

--- unit_tests/test_AddUsers.py
+++ unit_tests/test_AddUsers.py
@@ -12,8 +12,18 @@ class TestAddUsers(TestCase):
         self.InstructUser = Users.objects.create(group='Instructor')
         self.TAUser = Users.objects.create(group='TA')
 
-    def test_get(self):
+    def test_findAdmin(self):
+        self.assertEqual(self.AdminUser, Users.objects.get(group='Administrator'),
+                            msg='TestAddUsers:test_findAdmin Admin user does not exist in database')
+
+    def test_uniqueUsername(self):
+        self.assertEqual(self.existingUser, Users.objects.get(username='ottmakai000'),
+                         msg='TestAddUsers:test_uniqueUsername Existing user not found in database')
+
+    def test_existingUsername(self):
         pass
+
+
 
     def test_existingUser(self):
         self.assertEqual(self.existingUser, Users.objects.get(username='ottmakai000'),
@@ -24,10 +34,12 @@ class TestAddUsers(TestCase):
             Users.objects.get(password='badpassword')
 
     def test_goodPassword(self):
-        self.assertEqual(self.existingUser, Users.objects.get(password1='pass1'))
+        self.assertEqual(self.existingUser, Users.objects.get(password1='pass1'),
+                         msg='TestAddUsers:test_goodPassword ')
 
     def test_newUserAdmin(self):
-        self.assertEqual(self.AdminUser, Users.objects.get(group='Administrator'))
+        self.assertEqual(self.AdminUser, Users.objects.get(group='Administrator'),
+                         msg='TestAddUsers:test_newUserAdmin ')
 
     def test_newUserInstructor(self):
         self.assertEqual(self.InstructUser, Users.objects.get(group='Instructor'))
