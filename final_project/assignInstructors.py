a7449f38567a93ac1ebdcf56c21783de463f4ec9
Sana Fathima <78050187+sanfath@users.noreply.github.com>
2021-12-09 18:53:49 -0600

    Merge remote-tracking branch 'origin/sana-assigningTests' into sana-assigningTests

--- final_project/assignInstructors.py
+++ final_project/assignInstructors.py
@@ -8,12 +8,12 @@ class AdminAssignInstructors(object):
         self.assignInstructor = assignInstructor
 
     def assignInstructor(self, InstructorName):
-        #Precondition: isAdmin must be true and an admin must be logged in
-        #Postcondition: an instructor can now be assigned to a course
+        # Precondition: isAdmin must be true and an admin must be logged in
+        # Postcondition: an instructor can now be assigned to a course
         pass
 
     def setInstructor(self, courseName, instructorName):
-        #Precondition: courseName must be a course that is already created
-        #Precondition: instructorName must be an instructor with a created account
-        #Postcondition: the Instructor is now added to the course
+        # Precondition: courseName must be a course that is already created
+        # Precondition: instructorName must be an instructor with a created account
+        # Postcondition: the Instructor is now added to the course
         pass
--- final_project/createTAccount.py
+++ final_project/createTAccount.py
@@ -1,26 +1,34 @@
 from createaccounts import CreateAccountsInterface
 
+
 class CreateTAAccount(object, CreateAccountsInterface):
-    def getAccount(createNewAccount):
-        #Precondition: the account holder is an admin
-        #Precondition: the createNewAccount is true
-        #Postcondition: an account will be created
-        #createNewAccount: boolean to see if create account was selected by user or not
+    def __init__(self, createNewAccountTA, createNewAccountInstructor, createUser, createPassword, generateUserId):
+        self.generateUserId = generateUserId
+        self.createPassword = createPassword
+        self.createUser = createUser
+        self.createNewAccountInstructor = createNewAccountInstructor
+        self.createNewAccountTA = createNewAccountTA
+
+    def getAccount(self, createNewAccount):
+        # Precondition: the account holder is an admin
+        # Precondition: the createNewAccount is true
+        # Postcondition: an account will be created
+        # createNewAccount: boolean to see if create account was selected by user or not
         pass
 
-    def createAccount(createNewAccountTA, createNewAccountInstructor, createUser, createPassowrd):
-        #Precondition: the getAccount() is true
-        #Precondition: user selects either createNewAccountTA or createNewAccountInstructor to be true
-        #Postcondition: a new account with instructor permission/login, or ta login/persmission will be created
-        #Postcondition: a new username and password will be generated
-        #createUser: string to create user name
-        #createPassword: string to create password
-        #createNewAccountTA: boolean to make username and password specific for TA
-        #createNewAccountInstructor: boolean to make sure that Instructor is false and TA is true
+    def createAccount(self, createNewAccountTA, createNewAccountInstructor, createUser, createPassword):
+        # Precondition: the getAccount() is true
+        # Precondition: user selects either createNewAccountTA or createNewAccountInstructor to be true
+        # Postcondition: a new account with instructor permission/login, or ta login/persmission will be created
+        # Postcondition: a new username and password will be generated
+        # createUser: string to create user name
+        # createPassword: string to create password
+        # createNewAccountTA: boolean to make username and password specific for TA
+        # createNewAccountInstructor: boolean to make sure that Instructor is false and TA is true
         pass
 
-    def saveAccount(generateUserID):
-        #Precondition: make sure that username/password are valid
-        #Postcondition: match generateuserID to the username and password for the account created for TA
-        #generateUserID: the user account now has an individual id
+    def saveAccount(self, generateUserID):
+        # Precondition: make sure that username/password are valid
+        # Postcondition: match generateuserID to the username and password for the account created for TA
+        # generateUserID: the user account now has an individual id
         pass
