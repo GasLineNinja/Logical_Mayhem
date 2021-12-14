a24c3660e73ac5560cffd0ed27bb4626e19af1dc
UminekoVII <95431561+UminekoVII@users.noreply.github.com>
2021-12-13 22:24:00 -0600

    Removed "editcourses" line from addCourses.html, added a viewCourseDetails url in urls, added the viewCourseDetails html page, added the viewCourses html page, added viewCourses & viewCourseDetails views in views.py, added some bad tests in test_viewCourses.py

--- project_apps/views.py
+++ project_apps/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .models import Users, Courses, Labs
 
@@ -150,7 +150,16 @@ class ViewCourses(View):
 
     # display add users page
     def get(self, request):
-        return render(request, "", {})
+        if(Courses.objects.count() == 0):
+            return render(request, "viewCourses.html", {"message": "No courses have been added yet."})
+        else:
+            obj = Courses.objects.all()
+            return render(request, 'viewCourses.html', {'obj': obj})
+
+#class ViewCoursesDetails(View):
+#    def get(self, request, id):
+#        obj = get_object_or_404(Courses, pk=id)
+#        return render(request, 'viewCoursesDetails.html', {'obj': obj})
 
 class ViewUsers(View):
 
@@ -163,3 +172,7 @@ class Assignments(View):
     # display add users page
     def get(self, request):
         return render(request, "", {})
+
+def detail_page(request, id):
+    obj = get_object_or_404(Courses, pk=id)
+    return render(request, 'viewCourseDetails.html', {'obj': obj})
--- unit_tests/test_viewCourses.py
+++ unit_tests/test_viewCourses.py
@@ -1,24 +1,34 @@
+from django.shortcuts import render, get_object_or_404
 from django.test import TestCase
 from django.test import Client
-from project_apps.models import Users, Courses, Labs
+from project_apps.models import Users, Courses
+from django.utils import timezone
 
 class test_viewCourses(TestCase):
+    # Mohammad's Tests:
+    #def setup(self):
+        #self.client = Client()
+        #self.user0 = Users.objects.create(userName='purple', password1='purple')
+
+    #def test_noCourses(self, request):
+        #response = self.client.post('/', {'userName': 'purple', 'password2': 'purple'})
+        #self.assertEqual(response.url, '/homepage/')
+        #self.assertEqual(response.url, '/view_courses/')
+        #self.assertEqual(response.context['message'], 'No courses have been added yet.')
+        #return render(request, "viewCourses.html", {"message": "No courses have been added."})
+
     def setup(self):
-        self.client = Client();
+        self.client = Client()
 
         self.user1 = Users.objects.create(userName='mdstrand', group='administrator')
         self.user2 = Users.objects.create(userName='sghost', group='Instructor')
         self.user3 = Users.objects.create(userName='hbirdman', group='TA')
+        self.user0 = Users.objects.create(userName='purple', password1='purple')
 
         self.course1 = Courses.objects.create(courseName='CompSci 337 Systems Programming')
         self.course2 = Courses.objects.create(courseName='CompSci 361 Intro to Software Engineering')
         self.course3 = Courses.objects.create(courseName='INFOST 695 Ethical Hacking 1')
 
-    #Unit Tests
-
-
-
-    #Acceptance Tests
     def viewCoursesAdmin(self):
         response = self.user1.post('/', {'First name:', 'Space', 'Last name:', 'Ghost'})
         self.assertEquals(response.url, "/courseInfo/")
@@ -40,3 +50,6 @@ class test_viewCourses(TestCase):
         response = self.user2.post('/', {'First name:', 'Space', 'Last name:', 'Ghost'})
         self.assertEquals(response.url, "/courseInfo/")
 
+    #def test_moreThanOneCourse(self, request):
+        #obj = Courses.objects.all()
+        #return render(request, 'viewCourses.html', {'obj': obj})
