58234366429b23af7d2f9d9c8b7dad67bf9fac09
syedaafrah <68800827+syedaafrah@users.noreply.github.com>
2021-12-19 08:47:23 -0600

    Added to test but there are many errors I need to fix, and make sure that they are following reqs to fullfill the PBI

---
+++ classes/editTAAccount 2.py
@@ -0,0 +1,9 @@
+from editaccounts import EditAccountsInterface
+
+class EditTAAccount(EditAccountsInterface):
+    def editAccount(userID, group):
+        #Precondition: check isAdmin is true
+        #Precondition: enter the right userID, username, and password
+        #Postcondition: the user can make any edits for the account
+        #userID: int that acts as an individual ID for each account
+        pass
