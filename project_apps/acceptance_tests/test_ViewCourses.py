ec625f38ac0f076cc625174ecf0f06613a75b843
syedaafrah <68800827+syedaafrah@users.noreply.github.com>
2021-12-19 10:43:00 -0600

    Completed Aceptance (fixing one error)

--- project_apps/acceptance_tests/test_ViewCourses.py
+++ project_apps/acceptance_tests/test_ViewCourses.py
@@ -5,16 +5,13 @@ from project_apps.models import Users, Courses
 class viewCoursesSucess(TestCase):
         def setUp(self):
             self.client = Client()
-            self.user1 = Users.object.create(userName = '')
-            self.course1 = Courses.objects.create(courseName='CS361', courseNum='1', courseTime = "3", courseDay = "Mon", instructorName = "Jason", taName = 'Aproov', viewC = 'true')
+            self.course = Courses.objects.create(courseName='CS361', coursesNum='1', instructorName="Jason",
+                                                 viewC='true')
 
+        def test_courseViewed(self):
+            response = self.client.post('homepage.html', {'viewCourse': 'true'})
+            self.assertEqual(response.url, 'viewCourses', [self.course.courseName, self.course.courseNum, self.course.instructorName, self.course.viewC])
 
-       def test_courseViewed(self):
-           response = self.client.post('homepage.html', {'viewCourse' : 'true'})  # if correct username and password
-           self.assertEqual(response.url, 'viewCourses.html',[self.course1.courseName, self.course1.courseNum, self.course1.courseTime, self.course1.courseDay, self.course1.instructorName, self.course1.taName],  msg = 'the course is accurate')
-
-       def test_coursesNotViewd(self):
-           response = self.client.post('homepage.html', {'viewCourse' : 'false' })
-           self.assertEqual(response.url, 'viewCourses.html',[], msg = 'cannot access view courses')
-
-
+        def test_coursesNotViewd(self):
+            response = self.client.post('homepage.html', {'viewCourse': 'false'})
+            self.assertEqual(response.url, 'viewCourses.html', [], msg='cannot access view courses')
