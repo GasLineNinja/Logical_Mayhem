f59bae5415f680f60f482fbafbde100ad953b593
syedaafrah <68800827+syedaafrah@users.noreply.github.com>
2021-12-19 10:19:28 -0600

    Completed Aceptance (fixing one error)

--- project_apps/acceptance_tests/test_AddCourses.py
+++ project_apps/acceptance_tests/test_AddCourses.py
@@ -9,62 +9,62 @@ class MyTestCase(TestCase):
     def test_setUp(self):
         self.client = Client()
         self.course = Courses.objects.create(courseName='CS361', coursesNum = '1', instructorName="Jason",  viewC='true')
+        self.course2 = Courses.objects.create(courseName='CS377', coursesNum='1', instructorName="Jason", viewC='true')
 
-    def newUserItems(self):
-        response = self.client.get('/homepage/', {'name': self.client.name}) #if admin then
-        self.assertEqual(response.context['things'], [], msg='users list')
+    def newItems(self):
+        response = self.client.get('/homepage/', { 'group': 'Administrator'}) #if admin then add new item
+        self.assertEqual(response.context, 'addCourses' , msg='able to add courses')
 
-        response = self.client.post('addCourses', )  # if correct username and password
-        self.assertEqual(response.url, '/viewCourses/')  # assert that we should be in things page
+        response = self.client.post('addCourses', {'Courses.object.create.courseName': 'cs431'} )  # if correct username and password
+        self.assertEqual(response.context['addCourses'], [self.course.courseName, self.course2.courseName, self.course3.courseName],  msg='users list')  # assert that we should be in things page
 
-    def existingUserItems(self): #if the course already exists
-        response = self.client.post('/', {'name': 'bee', 'password': 'honey'})  # if correct username and password
-        self.assertEqual(response.url, '/things/')  # assert that we should be in things page
+    def existingItems(self): #if the course already exists
+        response = self.client.get('/homepage/', {'group': 'Administrator'})  # if admin then
+        self.assertEqual(response.context, 'addCourses', msg='able to add courses')
 
-        response = self.client.get('/things/', {'name': self.user.name})
-        self.assertEqual(response.context['things'], [self.stuff1.name, self.stuff2.name], msg='users list')
+        response = self.client.post('addCourses', {})  # if correct username and password
+        self.assertEqual(response.context['addCourses'], [self.course.Coursename, self.course2.name],
+                         msg='users list')  # assert that we should be in things page
 
-    def newUserWrongItems(self):#invalid items to make a course(view)
-        response = self.client.post('/', {'name': 'Jason', 'password': 'Rock'})  # if correct username and password
-        self.assertEqual(response.url, '/things/')  # assert that we should be in things page
+    def wrongUserInstrucot(self):#invalid items to make a course(view)
+        response = self.client.get('/homepage/', {'group': 'Instructor'})  # if admin then
+        self.assertEqual(response.context, '/homepage',  'addCourses', msg='able to add courses')
 
-        response = self.client.get('/things/', {'name': self.client.name})
-        self.assertEqual(response.context['things'], [self.stuff1.name, self.stuff2.name], msg='not the users list')
+    def wrongUserTA(self):#invalid items to make a course(view)
+        response = self.client.get('/homepage/', {'group': 'Ta'})  # if admin then
+        self.assertEqual(response.context, '/homepage',  'addCourses', msg='able to add courses')
+
 
     def existingUserWrongItems(self): #invalid display(view)
-        response = self.client.post('/', {''name': 'Jason', 'password': 'Rock''})  # if correct username and password
-        self.assertEqual(response.url, '/things/')  # assert that we should be in things page
+        response = self.client.get('/homepage/', {'group': 'Administrator'})  # if admin then
+        self.assertEqual(response.context, 'addCourses', msg='able to add courses')
 
-        response = self.client.get('/things/', {'name': self.user.name})
-        self.assertEqual(response.context['things'], [],  msg='not the users list')
+        response = self.client.post('addCourses', {})  # if correct username and password
+        self.assertEqual(response.context['addCourses'], ['class123'],
+                         msg='users list')  # assert that we should be in things page
 
-    def additems(self): # add items and update to views
-        response = self.client.post('/', {'name': 'amy', 'password': 'pass'})  # if correct username and password
-        self.assertEqual(response.url, '/things/')  # assert that we should be in things page
+    def addDuplicate(self): # add items and update to views
+        response = self.client.get('/homepage/', {'group': 'Administrator'})  # if admin then
+        self.assertEqual(response.context, 'addCourses', msg='able to add courses')
 
-        response = self.client.get('/things/', {'stuff': 'glasses'})
-        self.assertEqual(response.context['things'],  [self.stuff1.name, self.stuff2.name, 'glasses'],  msg='the users list')
+        response = self.client.post('addCourses', {'Courses.object.create.courseName': 'cs361'})  # if correct username and password
+        self.assertEqual(response.context['addCourses'], ['cs361', self.course2.name],
+                         msg='users list')  # assert that we should be in things page
 
-    def additemsNew(self): # additional items shown
-        response = self.client.post('/', {'name': 'bee', 'password': 'honey'})  # if correct username and password
-        self.assertEqual(response.url, '/things/')  # assert that we should be in things page
+    def missingItems(self): # additional items shown
+        response = self.client.get('/homepage/', {'group': 'Administrator'})  # if admin then
+        self.assertEqual(response.context, 'addCourses', msg='able to add courses')
 
-        response = self.client.get('/things/', {'stuff': 'pencil'})
-        self.assertEqual(response.context['things'], ['pencil'], msg= 'the users list')
+        response = self.client.post('addCourses', {})  # if correct username and password
+        self.assertEqual(response.context['addCourses'], [self.course.Coursename],
+                         msg='users list')  # assert that we should be in things page
 
     def donotadditems(self): #items that should not be added are added
-        response = self.client.post('/', {'name': 'amy', 'password': 'pass'})  # if correct username and password
-        self.assertEqual(response.url, '/things/')  # assert that we should be in things page
+        response = self.client.get('/homepage/', {'group': 'Administrator'})  # if admin then
+        self.assertEqual(response.context, 'addCourses', msg='able to add courses')
 
-        response = self.client.get('/things/', {'stuff': ''})
-        self.assertEqual(response.context['things'], [self.stuff1.name, self.stuff2.name],
-                         msg='dont add empty items')
-
-    def donotadditemsNew(self):
-        response = self.client.post('/', {'name': 'bee', 'password': 'honey'})  # if correct username and password
-        self.assertEqual(response.url, '/things/')  # assert that we should be in things page
-
-        response = self.client.get('/things/', {'stuff': ''})
-        self.assertEqual(response.context['things'], [], msg='dont add empty items')
-
+        response = self.client.post('addCourses',
+                                    {'@*&$N*(^&*SD'})  # if correct username and password
+        self.assertEqual(response.context['addCourses'], [self.course.Coursename, self.course2.name],
+                         msg='users list')  # assert that we should be in things page
 
